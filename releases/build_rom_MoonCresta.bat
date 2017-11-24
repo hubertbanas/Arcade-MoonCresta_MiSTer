cf2f1a52bc8a31ba25059187c10896e4a1f3faed
sorgelig <pour.garbage@gmail.com>
2017-11-25 02:41:32 +0800

    Load ROM from SD

---
+++ releases/build_rom_MoonCresta.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=mooncrgx.zip
+set ifiles=1+2+3+4+5+6+7+8+1h_1_10.bin+12.chr+9.chr+11.chr
+set  ofile=a.mncrst.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
